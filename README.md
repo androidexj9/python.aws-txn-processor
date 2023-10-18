a3861e7ec5654bc4ba4896ee9fcf79087297ae84
oscar.sanchez <androidexj9@gmail.com>
2023-10-17 19:05:13 -0600

    [ADD] Init project structure

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ $ source .venv/bin/activate
 
 ```
 (.venv) $ python3 -m pip install -r requirements.txt
+```
+```
 (.venv) $ python3 -m flask run
 ```
 
@@ -31,6 +33,8 @@ $ aws ecr create-repository --repository-name txn-processor
 ### Tag docker image:
 ```
 $ docker tag txn-processor:latest <repositoryUri>:latest
+```
+```
 $ docker tag txn-processor:latest 018042299534.dkr.ecr.us-east-1.amazonaws.com/txn-processor:latest
 ```
 
@@ -43,8 +47,13 @@ $ aws ecr get-login-password --region us-east-1 | docker login --username AWS --
 ### Push docker images to ECR:
 ```
 $ docker push <repositoryUri>:latest
+```
+```
 $ docker push 018042299534.dkr.ecr.us-east-1.amazonaws.com/txn-processor:latest
 ```
 
 ## Architecture
 ![alt text](https://github.com/androidexj9/python.aws-txn-processor/blob/main/architecture.png)
+
+## References
+https://docs.aws.amazon.com/lambda/latest/dg/with-s3-example.html
--- handler.py
+++ handler.py
@@ -1,22 +1,29 @@
 import json
 import urllib.parse
 import boto3
+import csv
+import io
 
-print('Loading function')
+print('Loading custom function')
 
 s3 = boto3.client('s3')
 
 
 def lambda_handler(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
+    #print("Received event: " + json.dumps(event, indent=2))
 
-    # Get the object from the event and show its content type
+    # Get the object from the event
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+
     try:
         response = s3.get_object(Bucket=bucket, Key=key)
-        print("CONTENT TYPE: " + response['ContentType'])
-        return response['ContentType']
+        data = response['Body'].read().decode('utf-8')
+        reader = csv.reader(io.StringIO(data))
+        next(reader)
+
+        for row in reader:
+            print(str.format("id: {} date: {} transaction: {}", row[0], row[1], row[2]))
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
